560a3b84f152fd2e86d5dbd39f551fe55ab15a59
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-05-25 21:46:42 +0900

    Update parse_data.py
    Made parse html funciton

--- parse_data.py
+++ parse_data.py
@@ -5,3 +5,13 @@ from bs4 import BeautifulSoup
 SCORE_DIR = "data/scores"
 box_scores=os.listdir(SCORE_DIR)
 box_scores = [os.path.join(SCORE_DIR, f) for f in box_scores if f.endswith(".html")]
+
+def parse_html(box_score):
+  with open(box_score) as f:
+    html = f.read()
+
+  soup = BeautifulSoup(html)
+  [s.decompose() for s in soup.select("tr.over_header")]
+  [s.decompose() for s in soup.select("tr.thead")]
+  return soup
+
