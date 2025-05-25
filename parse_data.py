05c9c4eec8aecce10fa83d805e8a11ad53304d18
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-05-25 21:41:36 +0900

    Update parse_data.py

--- parse_data.py
+++ parse_data.py
@@ -1,3 +1,7 @@
 import os
 import pandas as pd
 from bs4 import BeautifulSoup
+
+SCORE_DIR = "data/scores"
+box_scores=os.listdir(SCORE_DIR)
+box_scores = [os.path.join(SCORE_DIR, f) for f in box_scores if f.endswith(".html")]
