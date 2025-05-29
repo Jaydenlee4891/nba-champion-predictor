3dbf8d018569fdcde9782bf70d0678be3fcaa415
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-05-29 21:15:31 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+df = pd.read_csv("nba_games.csv", index_col=0)
+df = df.sort_values("date")
+df.reset_index(drop=True)
+del df["mp.1"]
+del df["mp_opp.1"]
+del df["index_opp"]
