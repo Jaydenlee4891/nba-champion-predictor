5aefce1f9ea0746041e40b6e293ccce82ebe0093
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-05-30 14:22:51 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.linear_model import RidgeClassifier
 
 df = pd.read_csv("nba_games.csv", index_col=0)
 df = df.sort_values("date")
@@ -19,3 +22,7 @@ df["target"] = df["target"].astype(int,errors="ignore")
 
 valid_columns = df.columns[~df.columns.isin(nulls.index)]
 df = df[valid_columns].copy()
+
+rr = RidgeClassifier(alpha=1)
+split = TimeSeriesSplit(n_splits=3)
+sfs = SequentialFeatureSelector(rr, n_features_to_select=30, direction="forward", cv=split)
