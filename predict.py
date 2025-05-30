afbb03ae187d03612a72fb882ec26c2f06150622
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-05-30 14:26:06 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import TimeSeriesSplit
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.linear_model import RidgeClassifier
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv("nba_games.csv", index_col=0)
 df = df.sort_values("date")
@@ -28,3 +29,5 @@ split = TimeSeriesSplit(n_splits=3)
 sfs = SequentialFeatureSelector(rr, n_features_to_select=30, direction="forward", cv=split)
 removed_columns = ["season","date","won","target","team","team_opp"]
 selected_columns = df.columns[~df.columns.isin(removed_columns)]
+scaler = MinMaxScaler()
+df[selected_columns] = scaler.fit_transform(df[selected_columns])
