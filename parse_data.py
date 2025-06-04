0e8285ce278fee60e611e4210879a6f6b9512ce0
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-04 22:22:46 +0900

    Update parse_data.py

--- parse_data.py
+++ parse_data.py
@@ -16,14 +16,15 @@ def parse_html(box_score):
     return soup
 
 def read_line_score(soup):
-  line_score = pd.read_html(str(soup), attrs={"id":"line_score"})[0]
-  cols = list(line_score.columns)
-  cols[0] = "team"
-  cols[-1] = "total"
-  line_score.columns = cols
-
-  line_score = line_score[["team", "total"]]
-  return line_score
+    line_score = pd.read_html(str(soup), attrs = {'id': 'line_score'})[0]
+    cols = list(line_score.columns)
+    cols[0] = "team"
+    cols[-1] = "total"
+    line_score.columns = cols
+    
+    line_score = line_score[["team", "total"]]
+    
+    return line_score
 
 def read_stats(soup, team, stat):
   df = pd.read_html(str(soup), attrs={"id":f"box-{team}-game-{stat}"}, index_col=0)[0]
