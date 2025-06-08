725b30ad603b9a7d871446cedfb28f12038ad67e
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-08 20:40:54 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,4 @@
 import pandas as pd
 from sklearn.model_selection import TimeSeriesSplit
-from sklearn.model_selection import TimeSeriesSplit
+from sklearn.linear_model import RidgeClassifier
 from sklearn.feature_selection import SequentialFeatureSelector
