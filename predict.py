bae0ca4f546da79ae4a92233bad831bcc8372540
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-08 20:44:26 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,3 +4,28 @@ from sklearn.linear_model import RidgeClassifier
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score
+
+def add_target(team):
+  team["target"] = team["won"].shift(-1)
+  return team
+
+def backtest(data, model, predictors, start=2, step=1):
+    all_predictions = []
+    
+    seasons = sorted(data["season"].unique())
+    
+    for i in range(start, len(seasons), step):
+        season = seasons[i]
+        train = data[data["season"] < season]
+        test = data[data["season"] == season]
+        
+        model.fit(train[predictors], train["target"])
+        
+        preds = model.predict(test[predictors])
+        preds = pd.Series(preds, index=test.index)
+        combined = pd.concat([test["target"], preds], axis=1)
+        combined.columns = ["actual", "prediction"]
+        
+        all_predictions.append(combined)
+    return pd.concat(all_predictions)
+
