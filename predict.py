1ff8468cafe762b98594435c8aa743f3f3a7659c
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-08 20:41:02 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,3 +2,4 @@ import pandas as pd
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.linear_model import RidgeClassifier
 from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.preprocessing import MinMaxScaler
