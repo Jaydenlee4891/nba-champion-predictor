223ef3a3df57ba50c325b42a4e67ecd3c9700172
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-10 15:41:31 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -28,6 +28,10 @@ def backtest(data, model, predictors, start=2, step=1):
         
         all_predictions.append(combined)
     return pd.concat(all_predictions)
+  
+def find_team_averages(team):
+  rolling = team.rolling(10).mean()
+  return rolling
 
 def shift_col(team,col_name):
   next_col = team[col_name].shift(-1)
