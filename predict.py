ee10a044652835adff9da2c0bf4276fb5d193841
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-10 15:39:16 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -29,3 +29,10 @@ def backtest(data, model, predictors, start=2, step=1):
         all_predictions.append(combined)
     return pd.concat(all_predictions)
 
+df = pd.read_csv("nba_games.csv", index_col=0)
+df = df.sort_values("date")
+df.reset_index(drop=True)
+del df["mp.1"]
+del df["mp_opp.1"]
+del df["index_opp"]
+
